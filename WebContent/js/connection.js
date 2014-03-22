a243b5eeae41e58b6f116ea9e2eb1aec4e4991cc
maxime <maximepvrt@gmail.com>
2014-03-22 15:35:13 +0100

    new connection

--- WebContent/js/connection.js
+++ WebContent/js/connection.js
@@ -2,19 +2,42 @@ window.Readily = window.Readily || {};
 
 (function(Readily){
 	
-	function Callback(status, param) {
+	var base = "http://localhost:8080/backend/webapi";
+	
+	function Callback(status, data) {
 		this.status = status;
-		this.param = param;
+		this.data = data;
 	}
 	
 	function httpCallback(verb, url, param, callback) {
-		// appel JQUERY
+		$.ajax({
+            async: false,
+            dataType: "json",
+            url: base,
+            success: function(data) {
+            	if (data.status == "OK")
+            		callback(new Callback("OK",data));
+				else
+					callback(new Callback("KO",data));
+            },
+            error: function(data) {
+            	 callback = new Callback("KO",data);
+            }
+        });
 	}
 	
 	function Connection() {
 		
 		this.login = function(login, password, callback) {
-			httpCall("POST", "/api/login", {login:login, password:password}, callback);
+			httpCall("POST", "/account/login", {login:login, password:password}, callback);
+		};
+		
+		this.newaccount = function(login, password, birthyear, gender, callback) {
+			httpCall("POST", "/account/newaccount", {login:login, password:password, birthyear:birthyear, gender:gender}, callback);
+		};
+		
+		this.newaccount = function(login, password, birthyear, gender, callback) {
+			httpCall("POST", "shortstory/find?lenght={length}&type={type}&number={nb}", "", callback);
 		};
 		
 	}
