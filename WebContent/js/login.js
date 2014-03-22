5927ed22d65ff00b376c39ef41d67371ab836b20
Rodrigo <reyes.rr@gmail.com>
2014-03-22 22:03:19 +0100

    navigation login/signup

--- WebContent/js/login.js
+++ WebContent/js/login.js
@@ -1,6 +1,6 @@
 (function(Readily){
 
-	Readily.Navigation.mkRoute("/signup", "#signup", "signup_tmpl.html", function(domId, template) {
+	Readily.Navigation.mkRoute("/login", "#login", "login_tmpl.html", function(domId, template) {
 		$(domId).html(template());
 	});
 
--- WebContent/js/signup.js
+++ WebContent/js/signup.js
@@ -1,7 +1,18 @@
 (function(Readily){
 
-	Readily.Navigation.mkRoute("/login", "#login", "login_tmpl.html", function(domId, template) {
+	Readily.Navigation.mkRoute("signup", "#signup", "signup_tmpl.html", function(domId, template) {
 		$(domId).html(template());
+		
+		var html = $(domId).html();
+		$(domId).find("form").submit(function(event) {
+			return false;
+		});
+		
+		$(domId).find("button").click(function(event) {
+			event.preventDefault();
+			// Submit en ajax/json
+			console.log("SUBMIT");
+		});
 	});
 
 })(window.Readily);
