a01e5390e809509089d9bff52d531dd9ca4e4318
Rodrigo <reyes.rr@gmail.com>
2014-03-22 12:51:48 +0100

    structure de l'objet Connection

--- WebContent/js/connection.js
+++ WebContent/js/connection.js
@@ -1,3 +1,23 @@
-(function(){
+window.Readily = window.Readily || {};
+
+(function(Readily){
 	
-})();
+	function Callback(status, param) {
+		
+	}
+	
+	function httpCallback(verb, url, param, callback) {
+		// appel JQUERY
+	}
+	
+	function Connection() {
+		
+		this.login = function(login, password, callback) {
+			httpCall("POST", "/api/login", {login:login, password:password}, callback);
+		};
+		
+	}
+
+	Readily.Connection = new Connection();
+	
+})(window.Readily);
